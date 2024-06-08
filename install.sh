8328a1cbd4a5a015f47eb103919c6452c42cdfee
Alexander Grenkov <a.grenkov@gmail.com>
2024-06-09 00:53:42 +0500

    fix: add git commands

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ $base_python_interpreter -m venv .venv
 source .venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
-echo "# Project ``" > README.md
+echo "# Project \`your-project-name\`" > README.md
+rm -rf .git
+git init
+git add .
+git commit -m "initial commit"
